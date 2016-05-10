826e8d9b8208cf6003fd067dfce4988ef9dd2afc
michusk <skoramichal3@hotmail.com>
2016-05-11 00:04:32 +0200

    funkcja edyt

--- nowe/src/javafxapplication3/EdytujProjektKierownikController.java
+++ nowe/src/javafxapplication3/EdytujProjektKierownikController.java
@@ -7,7 +7,13 @@ package javafxapplication3;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -27,15 +33,45 @@ public class EdytujProjektKierownikController implements Initializable {
     private Button wstecz;
     @FXML
     private Button wyloguj;
+    
+    
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        Connection conn;
+                        try{
+                           String id_pro = "01";
+                           String query = 
+                           "select postep_p, opis_p  from Projekty where  id_projektu='"+id_pro+"' limit 1";
+                           conn = new Driver().getConnection();
+                           Statement stmt = conn.createStatement();
+                           ResultSet rs = stmt.executeQuery(query);
+                           
+                        rs.next();
+                        
+                                int postep_p=rs.getInt("postep_p");
+                                String opis_p=rs.getString("opis_p");
+                                
+                                
+                                
+                                this.postep_p.setText(postep_p);
+                                this.opis_p.setText(postep_p);
+                           
+                       }
+                        
+                        catch (SQLException ex) {
+                            Logger.getLogger(kontoWidokController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+        
     }    
-
+    
+    
+    
     @FXML
     private void wstecz() throws IOException {
         
